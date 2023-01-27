11b43c52efd27603132d0274a7765b68a626d5d9
Marcus Gåhlin <Marcus@Marcuss-MacBook-Pro.local>
2023-01-27 23:06:26 +0100

    new build

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/IVIS23-project-1/',
+  base:'/IVIS23-Project-1/',
   plugins: [react()],
 })
