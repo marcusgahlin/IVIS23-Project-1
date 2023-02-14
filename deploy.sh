f6a379d4423fea4bafba1a50cd7de3a6a03c831a
Marcus Gåhlin <Marcus@Marcuss-MacBook-Pro.local>
2023-02-14 22:06:12 +0100

    GitHub Pages deploy script [2023-02-14@22:06:12 (CET)]

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# checkout on master???
+git checkout master
+
+# build the static app
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# bypass Jekyll processing
+echo > .nojekyll
+
+# Provide a helpful timestamp to commits 
+git add -A
+git commit -m "GitHub Pages deploy script
+[$(date '+%F@%T (%Z)')]"
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:marcusgahlin/IVIS23-Project-1.git main:gh-pages
+
+cd -
