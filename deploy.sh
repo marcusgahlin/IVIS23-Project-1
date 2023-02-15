7c192373c8686e5957de8594a1fde8d3bb53b300
Marcus Gåhlin <Marcus@Marcuss-MacBook-Pro.local>
2023-02-15 14:00:20 +0100

    deleted dist

--- deploy.sh
+++ deploy.sh
@@ -9,28 +9,24 @@ git checkout master
 # build the static app
 npm run build
 
+cd dist
+
 # bypass Jekyll processing
-echo > dist/.nojekyll
+echo > .nojekyll
 
 
 # Provide a helpful timestamp to commits 
 # this initiates a sub-git-repo (uggly work-around) to make sure we only get the dist files in the commit to github-pages
-# git init
+git init
 # create a branch in sub-git-repo
-# git checkout -B main
+git checkout -B main
 # do the commit (only contains dist)
-# git add -A
-#
-
-git add .
+git add -A
 git commit -m "GitHub Pages deploy script
-# [$(date '+%F@%T (%Z)')]"
-
-#
-git subtree push --prefix dist origin remotes/origin/gh-pages
+[$(date '+%F@%T (%Z)')]"
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# push the contents of the main (branch in sub-git-repo) to gh-pages branch
-# git push -f origin main:gh-pages
+# push the contents of the main (branch in sub-git-repo) to gh-pages branch. We need to specify the git@github address because in the sub-repo we don't have this as origin
+git push -f git@github.com:marcusgahlin/IVIS23-Project-1.git main:gh-pages
 
-# cd -
+cd -
